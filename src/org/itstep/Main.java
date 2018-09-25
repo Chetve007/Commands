8eac9ba00ccca48f0c583de239b58a00296d15c3
MarkShevelev <shevelev_m@itstep.org>
2018-09-25 22:02:39 +0300

    test fulfilled

--- src/org/itstep/Main.java
+++ src/org/itstep/Main.java
@@ -1,8 +1,27 @@
 package org.itstep;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 	// write your code here
+        try(Scanner sc = new Scanner(System.in)) {
+            NumberApplicationBuilder numAppBuilder = new NumberApplicationBuilder(new ArrayList<Integer>());
+            Map<String,Command> commands = new HashMap<>();
+            AbstractCommandAplication app = numAppBuilder
+                    .generateFrom("generate", new GenerateNumbersCommandFactory(1000, 1000))
+                    .sortFrom("sort", new SortNumbersCommandFactory())
+                    .saveFrom("save", new SaveNumbersCommandFactory(new File("./numbers.txt")))
+                    .buildUpon(sc, commands, () -> System.out.println("Неизвестная команда повторите ввод"));
+
+            commands.put("exit",()->app.stop());
+
+            app.loop();
+        }
     }
 }
