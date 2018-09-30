a4edd4eb0e552fa2d86352869daeec28e1433213
Aleksey <chetve007@mail.ru>
2018-09-30 12:06:55 +0300

    add sort in new Thread v2

--- src/org/itstep/SortNumbersCommand.java
+++ src/org/itstep/SortNumbersCommand.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class SortNumbersCommand implements Command, Runnable {
+public class SortNumbersCommand implements Command {
     private final List<Integer> lst;
     private final Writer answerTo;
 
@@ -17,22 +17,23 @@ public class SortNumbersCommand implements Command, Runnable {
 
     @Override
     public void exec() throws Exception {
+
         if (0 == lst.size()) {
             answerTo.write("Нечего сортировать!");
             answerTo.flush();
             return;
         }
 
+        Thread thread = new Thread(() -> {
+                for (int i =0; i != lst.size(); ++i)
+                    for (int j=0; j != lst.size()-1; ++j)
+                        if (lst.get(j) > lst.get(j+1))
+                            Collections.swap(lst,j,j+1);
+                try { answerTo.write("Сортировка окончена!"); answerTo.flush(); } catch (IOException e) { e.printStackTrace();}
+        });
+        thread.start();
+        thread.join();
+
         answerTo.write("Начинаю сортировку..."); answerTo.flush();
-
-    }
-
-    @Override
-    public void run() {
-        for (int i =0; i != lst.size(); ++i)
-            for (int j=0; j != lst.size()-1; ++j)
-                if (lst.get(j) > lst.get(j+1))
-                    Collections.swap(lst,j,j+1);
-        try { answerTo.write("Сортировка окончена!"); answerTo.flush(); } catch (IOException e) { e.printStackTrace(); }
     }
 }
